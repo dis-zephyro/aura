7e0fe3dfbbab0657296b4f757da2a20fcc4c96de
dis-zephyro <zephyro@yandex.ru>
2015-07-21 03:45:50 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -6,3 +6,21 @@ $(function(){
     $(mr_input).focus(function(){	 $(this).val()==$(this).data('stext')  ?   $(this).val(''): "0";	               	});
     $(mr_input).blur(function() {    $(this).val().length<=0               ?   $(this).val($(this).data('stext')): "0"; });
 });
+
+
+$(document).ready(function() {
+    $('.reply-hidden').hide();
+
+    $('.reply-more').click(function(event) {
+        event.preventDefault(); // Для того чтобы при нажатии на ссылку не кидало вверх
+        $('.reply-hidden').slideToggle();
+        $('.reply-more').hide();
+    });
+});
+
+
+// Модальное окно
+
+$(".btn-callback, .btn-popup").fancybox({
+    "padding" : 0
+});
